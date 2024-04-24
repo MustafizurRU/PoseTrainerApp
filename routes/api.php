36c0cd1e8762987f509b57870ea1a08fac55c6b6
MustafizurRU <mmrsaju.cd@gmail.com>
2024-04-24 12:27:18 +0900

    first commit

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use App\Http\Controllers\Api\ApiController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+//login
+Route::post('/login', [ApiController::class, 'login']);
+//register
+Route::post('/register', [ApiController::class, 'register']);
